caf203feee1499ed1d4e924e831ec5d1f2ed65d4
Sarthak Singh <sarthaksingh.077@gmail.com>
2021-08-23 11:16:48 +0530

    first commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,7 +7,7 @@ const Content = () => {
     return (
         <section className="content">
             <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab}/>
-            <Tasks />
+            <Tasks selectedTab={selectedTab}/>
         </section>
     );
 };
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,6 +2,10 @@ import React,{ useState } from "react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import "react-day-picker/lib/style.css";
 import dateFnsFormat from "date-fns/format";
+import isAfter from "date-fns/isAfter";
+import isBefore from "date-fns/isBefore";
+import addDays from "date-fns/addDays";
+import isToday from "date-fns/isToday";
 
 const FORMAT = "dd/MM/yyyy";
 
@@ -49,7 +53,39 @@ const AddTasks = ({ onCancel, onAddTask }) => {
     );
 };
 
-const Tasks = () => {
+const Tasks_Header_Mapping = {
+    INBOX: "Inbox",
+    TODAY: "Today",
+    NEXT_7: "Next 7 days",
+};
+
+const TaskItems = ({ selectedTab, tasks }) => {
+    let tasksToRender = [...tasks];
+    if (selectedTab === "NEXT_7") {
+        tasksToRender = tasksToRender.filter(
+            (task) =>
+                isAfter(task.date, new Date()) &&
+                isBefore(task.date, addDays(new Date(), 7))
+        );
+    }
+    
+    if (selectedTab === "TODAY") {
+        tasksToRender = tasksToRender.filter((task) => isToday(task.date))      
+    }
+
+    return (
+        <div className="task-items-container">
+            {tasksToRender.map((task) => (
+                <div className="task-item">
+                    <p>{task.text}</p>
+                    <p>{dateFnsFormat(new Date(task.date), FORMAT)}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+const Tasks = ({selectedTab}) => {
     const [showAddTask, setShowAddTask] = useState(false);
     const [tasks, setTasks] = useState([]);
 
@@ -59,14 +95,16 @@ const Tasks = () => {
     };
     return (
         <div className="tasks">
-            <h1>Inbox</h1>
-            <div
-                className="add-task-btn"
-                onClick={() => setShowAddTask((prevState) => !prevState)}
-            >
-                <span className="plus">+</span>
-                <span className="add-task-text">Add Task</span>
-            </div>
+            <h1>{Tasks_Header_Mapping[selectedTab]}</h1>
+            {selectedTab === "INBOX" ? (
+                <div
+                    className="add-task-btn"
+                    onClick={() => setShowAddTask((prevState) => !prevState)}
+                >
+                    <span className="plus">+</span>
+                    <span className="add-task-text">Add Task</span>
+                </div>) : null
+            }
             {showAddTask && (
                 <AddTasks
                     onAddTask={addNewTask}
@@ -74,14 +112,9 @@ const Tasks = () => {
                 />
             )}
             {tasks.length > 0 ? (
-                tasks.map((task) => (
-                    <p>
-                        {task.text}
-                        {" - "}
-                        {dateFnsFormat(new Date(task.date),FORMAT)}
-                    </p>
-                ))
-            ) : (<p> No Tasks Yet</p>
+                <TaskItems tasks={tasks} selectedTab={selectedTab} />
+            ) : (
+            <p> No Tasks Yet</p>
             )}
         </div>
     );
